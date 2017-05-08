069b0cca2d9c70c725e7c54f0c37cf29e805d7e6
Zixuan <gzzxmail@gmail.com>
2017-05-07 23:18:38 -0700

    improve

--- git_related/show_git_branch.sh
+++ git_related/show_git_branch.sh
@@ -1,3 +1,7 @@
+######################################################
+# source this file at the end of ~/.bashrc
+# this will show git branch and status in bash prompt
+######################################################
 parse_git_branch() {
     local branch=$(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')
     local status=$(if [ $(git status -s 2> /dev/null | wc -l) -ne 0 ]; then echo "*"; fi)
@@ -7,5 +11,4 @@ parse_git_branch() {
 }
 GIT_BRANCH="\[\033[32m\]\$(parse_git_branch)\[\033[00m\]"
 
-# example:
-#PS1="${debian_chroot:+($debian_chroot)}\u@macbook:\W\$ ${GIT_BRANCH} "
+PS1=$PS1" ${GIT_BRANCH} "
